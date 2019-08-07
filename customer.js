74414dbc2a763a08f05bca2d141ae18ac4021db7
Zac Morton <zmorton8057@gmail.com>
2019-08-07 15:54:13 -0700

    Added inquirer promot for id and quantity but still need to link it to mySQL

--- customer.js
+++ customer.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
     database: "bamazonDB"
 });
 
-connection.connect(function (err) {
+connection.connect(function(err) {
     if (err) throw err;
     console.log('connected as id ' + connection.threadId);
 
@@ -21,24 +21,25 @@ connection.connect(function (err) {
 /////////cli-table design for displaying the inventory
 function table() {
 
-    // instantiate
+    // Creates a table with colum titles of ID, Item, Price, and Stock
     var table = new Table({
-        head: ['ID', 'Item', 'Price', 'Stock'],
+        head: ['ID', 'Item', 'Price', 'Available Stock'],
         colWidths: [10, 30, 30, 30]
     });
 
     displayInventory();
     ////////// retrieves inventory from mySQL with a query
     function displayInventory() {
-        connection.query("SELECT * FROM products", function (err, res) {
+        connection.query("SELECT * FROM products", function(err, res) {
             if (err) throw err;
 
             for (var i = 0; i < res.length; i++) {
+                ////////Assigns SQL data to variables so it can be pushed to a table for display purposes
                 var itemID = res[i].id,
                     productName = res[i].product_name,
                     price = `$${res[i].price}`,
                     quantity = res[i].quantity;
-
+                //////////Pushes sql data to Table CLI
                 table.push(
                     [itemID, productName, price, quantity]
                 );
@@ -46,6 +47,28 @@ function table() {
             console.log("========================================= Welcome to Bamazon ===========================================")
             console.log("============================================= Inventory ================================================")
             console.log(table.toString());
+            selectionMake();
         });
     }
-    }
+}
+
+var selectionMake = function(){
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "itemID",
+                message: "Input the ID of Item you wish to purchase",
+            },
+            {
+                type: "input",
+                name: "userQuantity",
+                message: "Input the quantity you wish to purchase",
+            }/* Pass your questions in here */
+        ])
+        .then(function(userPurchase) {
+            connection.query("SELECT * FROM products WHERE item_id=?", userPurchase.inputId, function(err, res){
+                
+                    })
+                })
+            }
